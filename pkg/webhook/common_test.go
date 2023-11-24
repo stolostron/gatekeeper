897c013d27be5ce1868d227c6d6f7e007e6a0d86
Yi Rae Kim <yikim@redhat.com>
2023-11-24 16:12:51 -0500

    Remove TestCongifurewebhookServer
    Signed-off-by: Yi Rae Kim <yikim@redhat.com>

--- pkg/webhook/common_test.go
+++ pkg/webhook/common_test.go
@@ -15,13 +15,11 @@ import (
 	"net"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
 type chanWriter chan string
@@ -29,39 +27,6 @@ type chanWriter chan string
 func (w chanWriter) Write(p []byte) (n int, err error) {
 	w <- string(p)
 	return len(p), nil
-}
-
-func TestCongifureWebhookServer(t *testing.T) {
-	expectedServer := &webhook.Server{
-		TLSMinVersion: "1.2",
-	}
-
-	if *clientCAName != "" {
-		expectedServer.ClientCAName = *clientCAName
-	}
-
-	tc := []struct {
-		Name           string
-		Server         *webhook.Server
-		ExpectedServer *webhook.Server
-	}{
-		{
-			Name:           "Wbhook server config",
-			Server:         &webhook.Server{},
-			ExpectedServer: expectedServer,
-		},
-	}
-
-	for _, tt := range tc {
-		t.Run(tt.Name, func(t *testing.T) {
-			server := congifureWebhookServer(tt.Server)
-			expectedServer.TLSOpts = server.TLSOpts
-
-			if !reflect.DeepEqual(tt.ExpectedServer, server) {
-				t.Errorf(fmt.Sprintf("got %#v, want %#v", server, tt.ExpectedServer))
-			}
-		})
-	}
 }
 
 func TestTLSConfig(t *testing.T) {
